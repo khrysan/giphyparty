2f8ca47979f4e908a0adef9089a397643230c4ac
Christine Capili <khrys@khrysan.local>
2023-10-24 19:12:29 -0700

    added event listener on search, delete, and DOMLoad

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 console.log("Let's get this party started!");
 
-const authKey = "bHVRLkmnJxO8DogGGftrhVzzosgQAIbd";
+const api_key = "bHVRLkmnJxO8DogGGftrhVzzosgQAIbd";
+
+document.querySelector("#submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  let tag = document.querySelector("#gifSearch").value;
+  getGif(tag);
+  document.querySelector("#form").reset();
+});
+
+document.querySelector("#clearGifs").addEventListener("click", function (e) {
+  e.preventDefault();
+  const container = document.querySelector("#container");
+  container.innerHTML = "";
+});
+
+async function getGif(tag) {
+  let res = await axios.get("http://api.giphy.com/v1/gifs/random", {
+    params: { api_key, tag },
+  });
+  const img = document.createElement("img");
+  img.src = res.data.data.images.original.url;
+  document.querySelector("#container").append(img);
+  console.log(res);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  rdmPlaceholder();
+});
+
+async function rdmPlaceholder() {
+  let res = await axios.get("http://api.giphy.com/v1/gifs/random", {
+    params: { api_key },
+  });
+  console.log(res);
+  const rdmTag = document.querySelector("#gifSearch");
+  rdmTag.setAttribute("placeholder", res.data.data.title);
+}
